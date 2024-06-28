6530933da686fbf4eacfd23bd2ebe05adaf28ece
Amrit Raj <108748556+amroodh@users.noreply.github.com>
2024-06-28 07:59:45 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/navbar/hero/Hero";
+import Parallax from "./components/parallax/parallax";
+import Contact from "./components/contact/Contact";
+import Services from "./components/Services/Services";
+import Cursor from "./components/cursor/cursor";
+import Portfolio from "./components/portfolio/portfolio";
+
+const App = () => {
+  return (
+    <div>
+      <Cursor />
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
+      <section>
+        <Services />
+      </section>
+      <section id="portfolio">
+        <Parallax type="Portfolio" />
+      </section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
+    </div>
+  );
+};
+
+export default App;
